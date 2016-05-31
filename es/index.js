3dbdfb3b0deb92d146d6c1d05bb8c477c1f8e5b0
Oleg Orlov <mail@orlovoleg.com>
2016-05-31 17:05:43 +0300

    build

--- es/index.js
+++ es/index.js
@@ -1,40 +1,21 @@
-'use strict';
-
-exports.__esModule = true;
-
-var _keys = require('babel-runtime/core-js/object/keys');
-
-var _keys2 = _interopRequireDefault(_keys);
-
-var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
-
-var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
-
-var _possibleConstructorReturn2 = require('babel-runtime/helpers/possibleConstructorReturn');
-
-var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
-
-var _inherits2 = require('babel-runtime/helpers/inherits');
-
-var _inherits3 = _interopRequireDefault(_inherits2);
-
-var _tango = require('@eagle/tango');
-
-var _immutable = require('immutable');
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+import _Object$keys from 'babel-runtime/core-js/object/keys';
+import _classCallCheck from 'babel-runtime/helpers/classCallCheck';
+import _possibleConstructorReturn from 'babel-runtime/helpers/possibleConstructorReturn';
+import _inherits from 'babel-runtime/helpers/inherits';
+import { Component } from '@eagle/tango';
+import { fromJS, Iterable, List, Map } from 'immutable';
 
 var DataComponent = function (_Component) {
-  (0, _inherits3.default)(DataComponent, _Component);
+  _inherits(DataComponent, _Component);
 
   function DataComponent() {
-    (0, _classCallCheck3.default)(this, DataComponent);
+    _classCallCheck(this, DataComponent);
 
     for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
       args[_key] = arguments[_key];
     }
 
-    var _this = (0, _possibleConstructorReturn3.default)(this, _Component.call.apply(_Component, [this].concat(args)));
+    var _this = _possibleConstructorReturn(this, _Component.call.apply(_Component, [this].concat(args)));
 
     if (!_this.constructor.storeProps) {
       throw new Error('DataComponent requires storeProps to be defined! Did you forget to use the connect decorator?');
@@ -60,27 +41,27 @@ var DataComponent = function (_Component) {
     var _this2 = this;
 
     // has keys that are either undefined/null or have a pending = true key
-    return (0, _immutable.fromJS)(this.constructor.storeProps).reduce(function (prev, cursor, prop) {
+    return fromJS(this.constructor.storeProps).reduce(function (prev, cursor, prop) {
       return _this2.isPropResolving(prop) ? prev.push(prop) : prev;
-    }, (0, _immutable.List)());
+    }, List());
   };
 
   DataComponent.prototype.getErrors = function getErrors() {
     var _this3 = this;
 
     // has keys that have an error = data key
-    return (0, _immutable.fromJS)(this.constructor.storeProps).reduce(function (prev, cursor, prop) {
+    return fromJS(this.constructor.storeProps).reduce(function (prev, cursor, prop) {
       return _this3.isPropErrored(prop) ? prev.set(prop, _this3.props[prop].get('error')) : prev;
-    }, (0, _immutable.Map)());
+    }, Map());
   };
 
   DataComponent.prototype.getResolvedData = function getResolvedData() {
     var _this4 = this;
 
-    return (0, _keys2.default)(this.constructor.storeProps).reduce(function (prev, prop) {
+    return _Object$keys(this.constructor.storeProps).reduce(function (prev, prop) {
       var val = _this4.props[prop];
       if (!_this4.isPropResolving(prop)) {
-        prev[prop] = _immutable.Iterable.isIterable(val) ? val.get('data') || val : val;
+        prev[prop] = Iterable.isIterable(val) ? val.get('data') || val : val;
       }
       return prev;
     }, {});
@@ -94,11 +75,11 @@ var DataComponent = function (_Component) {
   };
 
   DataComponent.prototype.isPropResolving = function isPropResolving(prop) {
-    return this.props[prop] == null || _immutable.Iterable.isIterable(this.props[prop]) && this.props[prop].get('pending') === true;
+    return this.props[prop] == null || Iterable.isIterable(this.props[prop]) && this.props[prop].get('pending') === true;
   };
 
   DataComponent.prototype.isPropErrored = function isPropErrored(prop) {
-    return _immutable.Iterable.isIterable(this.props[prop]) && this.props[prop].get('error') != null;
+    return Iterable.isIterable(this.props[prop]) && this.props[prop].get('error') != null;
   };
 
   DataComponent.prototype.isResolving = function isResolving() {
@@ -136,7 +117,6 @@ var DataComponent = function (_Component) {
   };
 
   return DataComponent;
-}(_tango.Component);
+}(Component);
 
-exports.default = DataComponent;
-module.exports = exports['default'];
+export default DataComponent;
